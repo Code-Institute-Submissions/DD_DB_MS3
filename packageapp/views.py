0390dab30b938321a20e38039c096bffa3a60262
CruzZFire <a.cruzana88@gmail.com>
2020-07-08 17:28:20 +0200

    Added all planed view routes and templates without functions for real scope of the project

--- packageapp/views.py
+++ packageapp/views.py
@@ -5,3 +5,23 @@ from flask import render_template
 @app.route("/index")
 def index():
     return render_template("index.html", title="Index")
+
+@app.route("/products")
+def products():
+    return render_template("products.html", title="My products")
+
+@app.route("/editproduct")
+def editproduct():
+    return render_template("editproduct.html", title="Edit Product")
+
+@app.route("/addproduct")
+def addproduct():
+    return render_template("addproduct.html", title="Add Product")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html", title="")
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html", title="")
