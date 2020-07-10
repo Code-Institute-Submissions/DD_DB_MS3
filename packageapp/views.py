6940f631d0fce0b007bc2ed8cd30e67222325d82
CruzZFire <a.cruzana88@gmail.com>
2020-07-10 17:51:31 +0200

    Added validation to signin.html form, changed href to url_for on base and index

--- packageapp/views.py
+++ packageapp/views.py
@@ -1,29 +1,32 @@
 from packageapp import app, mongodb
-from flask import render_template, redirect, request, url_for
-from packageapp.forms import SigninForm
+from flask import render_template, redirect, request, url_for, flash
+from packageapp.forms import SigninForm, SignupForm
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html", title="Index", makeups=mongodb.db.makeups.find())
 
-@app.route("/products")
+@app.route("/products", methods=["GET", "POST"])
 def products():
     return render_template("products.html", title="My products")
 
-@app.route("/editproduct")
+@app.route("/editproduct", methods=["GET", "POST"])
 def editproduct():
     return render_template("editproduct.html", title="Edit Product")
 
-@app.route("/addproduct")
+@app.route("/addproduct", methods=["GET", "POST"])
 def addproduct():
     return render_template("addproduct.html", title="Add Product")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     return render_template("signup.html", title="")
 
-@app.route("/signin")
+@app.route("/signin", methods=["GET", "POST"])
 def signin():
     form = SigninForm()
+    if form.validate_on_submit():
+        flash("Login successfull, {}".format(form.user_name.data))
+        return redirect("/index")
     return render_template("signin.html", title="Sign In", form=form)
